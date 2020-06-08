c17398f1648d85d49e7eb04a23efa93dc58d90f3
Igor Sowinski <igor.s@devpark.pl>
2020-06-08 07:16:35 +0200

    first commity

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+    public function testExample2()
+    {
+        $this->assertTrue(false);
+    }
+}
